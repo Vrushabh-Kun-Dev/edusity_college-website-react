1fb7def985ff571689195182aa273545773e3e5e
Vrushabh-Kun-Dev <vrushabh.kun.dev@gmail.com>
2025-08-24 11:30:42 +0530

    Programs Section is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle="Our Program" title="What We Offer"/>
+        <Programs />
+      </div>
     </>
-  )
+  );
 }
 
 
